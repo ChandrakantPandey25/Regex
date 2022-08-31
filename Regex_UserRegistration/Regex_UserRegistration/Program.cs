a0e41ff927833b6cf6a676d0812f3d68b3bb1c17
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-30 19:26:01 -0700

    Added Validate First Name

--- Regex_UserRegistration/Regex_UserRegistration/Program.cs
+++ Regex_UserRegistration/Regex_UserRegistration/Program.cs
@@ -6,7 +6,17 @@ namespace Regex_UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Regex Program");
+            Console.WriteLine("Enter 1 to validate firstname");
+            switch(Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    UC1_FirstName firstname = new UC1_FirstName();
+                    firstname.FirstName();
+                    break;
+
+
+            }
         }
     }
 }
--- Regex_UserRegistration/Regex_UserRegistration/UC1_FirstName.cs
+++ Regex_UserRegistration/Regex_UserRegistration/UC1_FirstName.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Regex_UserRegistration
 {
-    class UC1_FirstName
+    public class UC1_FirstName
     {
+        public void FirstName()
+        {
+            Console.WriteLine("Enter the name");
+            string firstName = Console.ReadLine();
+            string pattern = "(^[^a-z][A-z]{1}[a-z]{2,}$)";
+            if (Regex.IsMatch(firstName, pattern))
+                Console.WriteLine("entered name is valid");
+            else
+            {
+                Console.WriteLine("Entered name is Invalid... please give first character in capital and minimum 3 characters name");
+                FirstName();
+            }
+        }
     }
 }

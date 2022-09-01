81244d539497b292374d52748f4cd6e21cf34116
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-31 22:59:58 -0700

    Added Email validation

--- Regex_UserRegistration/Regex_UserRegistration/Program.cs
+++ Regex_UserRegistration/Regex_UserRegistration/Program.cs
@@ -9,6 +9,7 @@ namespace Regex_UserRegistration
             Console.WriteLine("Welcome to Regex Program");
             Console.WriteLine("Enter 1 to validate firstname");
             Console.WriteLine("Enter 2 to validate lastname");
+            Console.WriteLine("Enter 3 to validate email");
             switch(Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -18,6 +19,10 @@ namespace Regex_UserRegistration
                 case 2:
                     UC2_LastName lastName = new UC2_LastName();
                     lastName.LastName();
+                    break;
+                case 3:
+                    UC3_ValidEmail validEmail = new UC3_ValidEmail();
+                    validEmail.ValidEmail();
                     break;
 
 
---
+++ Regex_UserRegistration/Regex_UserRegistration/UC3_ValidEmail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex_UserRegistration
+{
+    class UC3_ValidEmail
+    {
+        public void ValidEmail()
+        {
+            Console.WriteLine("Enter email");
+            string email = Console.ReadLine();
+            string pattern = "^[a-zA-Z0-9]{3,25}([.][0-9a-zA-Z]{1,7})*@[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.][a-zA-Z]{2,3})*$";
+            
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine("entered email is valid");               
+                ValidEmail();
+            }
+            else
+            {
+                Console.WriteLine("entered email invalid");              
+                ValidEmail();
+            }
+        }
+    }
+}
